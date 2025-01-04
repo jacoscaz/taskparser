22acad5e1ef2fcd71490c954c5181e2551de6e35
Jacopo Scazzosi <jacopo@scazzosi.com>
2025-01-04 20:16:59 +0100

    iso-like date when printing out to csv

--- src/renderers/csv.ts
+++ src/renderers/csv.ts
@@ -5,7 +5,12 @@ import { stringify } from 'csv-stringify/sync';
 
 export const renderCSV: RenderItemsFn = (items, show_tags) => {
   process.stdout.write(stringify(
-    items.map(({ tags }) => tags),
+    items.map(({ tags }) => {
+      if (tags.date) {
+        tags.date = `${tags.date.slice(0, 4)}-${tags.date.slice(4, 6)}-${tags.date.slice(6, 8)}`;
+      }
+      return tags;
+    }),
     { columns: show_tags, header: true },
   ));
 };
